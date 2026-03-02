2a9b52e947fd9f54b7d530b012b882f735fca46d
Diya Patel <diyapatel41205@gmail.com>
2026-03-02 16:28:35 -0600

    Update mpu.js

--- mpu.js
+++ mpu.js
@@ -1,1 +1,38 @@
+import { cpu } from "./cpu.js";
 
+export const regions = {
+  user: { start: 0x0200, end: 0x02FF },
+  kernel: { start: 0x0300, end: 0x033F },
+  protected: { start: 0x0340, end: 0x03FF }
+};
+
+export const faultLog = {
+  count: 0,
+  lastAddress: null,
+  lastType: null
+};
+
+export function safeMemAccess(addr, type, value = 0) {
+  if (addr >= regions.protected.start && addr <= regions.protected.end) {
+    fault(addr, type);
+  }
+
+  if (addr >= regions.kernel.start && addr <= regions.kernel.end) {
+    if (cpu.mode !== "KERNEL") fault(addr, type);
+  }
+
+  if (type === "WRITE") cpu.memory[addr] = value;
+}
+
+function fault(addr, type) {
+  faultLog.count++;
+  faultLog.lastAddress = addr;
+  faultLog.lastType = type;
+  throw new Error("MPU FAULT");
+}
+
+export function resetFaults() {
+  faultLog.count = 0;
+  faultLog.lastAddress = null;
+  faultLog.lastType = null;
+}
